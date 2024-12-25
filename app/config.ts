c8c10b8c3e9bd2a39bf535a1d3852500229b6c34
Tanjim Pranto <tanjim.outcast@gmail.com>
2024-12-25 22:01:20 +0200

    Initial commit

--- app/config.ts
+++ app/config.ts
@@ -1,16 +1,14 @@
 export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  title: "Tanjim's Portfolio",
+  name: "Tanjim Ahmed Pranto",
   ogImage: "/opengraph-image.png",
   description:
     "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  github: "https://github.com/tanjimahmedpranto",
+  linkedin: "https://www.linkedin.com/in/tanjimahmedpranto/",
+  email: "mailto:tanjim.outcast@gmail.com",
 };
